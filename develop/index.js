953d19d4cb3272916e539ee8e70dc7bf3aadc04f
H Lowe <haleylowe0@gmail.com>
2020-09-26 21:58:15 -0700

    added connection to database

--- develop/index.js
+++ develop/index.js
@@ -1,2 +1,18 @@
+//dependencies
 const mysql = require("mysql");
 const inquirer = require("inquirer");
+
+//create connection to database
+let connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: '',
+    database: 'employeeManagement'
+});
+ connection.connect(err => {
+     if (err) {
+         return console.error('error' + err.message);
+     }
+    promptQuestions();
+ })
